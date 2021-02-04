626047c0a99d125834a571c8b02d2d14624598ea
tusharjain8177 <67454862+tusharjain8177@users.noreply.github.com>
2021-02-04 09:34:34 +0530

    Update coming.js
    Update

--- js/coming.js
+++ js/coming.js
@@ -11,10 +11,10 @@ var x = setInterval(function() {
   var distance = countDownDate - now;
 
   // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  var days = Math.floor(distance / (10000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (10000 * 60 * 60 * 24)) / (10000 * 60 * 60));
+  var minutes = Math.floor((distance % (10000 * 60 * 60)) / (10000 * 60));
+  var seconds = Math.floor((distance % (10000 * 60)) / 10000);
 
   // Display the result in an element with id="demo"
   document.getElementById("demo").innerHTML = days + "d " + hours + "h "
